678bfd57de459c648f9fe2c13818b38a6d30a348
kento-yoshidu <kento yoshizu>
2021-02-27 20:39:05 +0900

    Update

--- app.js
+++ app.js
@@ -1,11 +1,13 @@
 const express = require('express');
 const { graphqlHTTP } = require('express-graphql');
+const schema = require('./schema/schema');
 
 const app = express();
 
 // bind express with graphql
 app.use('/graphql', graphqlHTTP({
-    // pass in a schema property
+  schema,
+  graphiql: true
 }));
 
 app.listen(4000, () => {
--- schema/schema.js
+++ schema/schema.js
@@ -1,12 +1,38 @@
-const graphq = require('graphql');
+const graphql = require('graphql');
+const _ = require('lodash');
 
-const { GraphQLObjectType, GraphQLString } = graphql;
+const { GraphQLObjectType, GraphQLString, GraphQLSchema } = graphql;
 
-const BookType = new GraphqlObjectType({
+// dummy data
+
+const book = [
+  { name: "Kento", genre: "Fantasy", id: "1"},
+  { name: "hikari", genre: "Fantasy", id: "2"},
+  { name: "Kento", genre: "Sci", id: "3"},
+];
+
+const BookType = new GraphQLObjectType({
   name: 'Book',
   fields: () => ({
     id: { type: GraphQLString},
     name: { type: GraphQLString},
     genre: { type: GraphQLString}
   })
-})
+});
+
+const RootQuery = new GraphQLObjectType({
+  name: 'RootQueryTypes',
+  fields: {
+    book: {
+      type: BookType,
+      args: { id: { type: GraphQLString } },
+      resolve(parent, args) {
+        return _.find(book, { id: args.id})
+      }
+    }
+  }
+});
+
+module.exports = new GraphQLSchema({
+  query: RootQuery
+});
